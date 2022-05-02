d97e34c63f8ec9b608e465bb69196792a8788e4f
wxytop <290220048@qq.com>
2022-05-02 18:46:47 +0800

    更新 pxt.json, main.blocks, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
 // 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
+//% color="#0f80ea" weight=10 icon="\uf1d1"
+namespace rkSensorExt {
+    //% blockId=robotbit_matrix_clear block="Matrix Clear"
+    //% weight=65
+    //% blockGap=50
+    export function MatrixClear(): void {
+    
+    }
+}
