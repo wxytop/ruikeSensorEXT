c21cd8623eb84b7ecb362b956418c0b26b6e759c
wxytop <290220048@qq.com>
2022-05-07 15:15:34 +0800

    更新 pxt.json, main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+basic.showIcon(IconNames.Heart)
+rkSensorExt.MP3_init(SerialPin.P13, SerialPin.P12)
+rkSensorExt.MP3_volume_set(25)
+basic.forever(function () {
+    rkSensorExt.MP3_play(rkSensorExt.playType.Last)
+    basic.pause(2000)
+})
